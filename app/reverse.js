6051c9ca74bc5f06b0dc62f3f7616a45d62cfbfa
LB123658 <79328251+LB123658@users.noreply.github.com>
2021-07-07 19:52:03 -0500

    Create reverse.js

---
+++ app/reverse.js
@@ -0,0 +1,13 @@
+alert("UNKRAFTED reverse color add-on running.\nPress 'i' for more information.");
+
+var back = document.getElementById("background");
+var char = document.getElementById("char");
+var sun = document.getElementById("sun");
+var ground = document.getElementById("ground");
+var bomb = document.getElementById("bomb");
+
+back.style.background = "purple";
+char.style.borderRadius = "50%";
+sun.style.background = "red";
+ground.style.background = "#FFC000";
+bomb.style.background = "white";
